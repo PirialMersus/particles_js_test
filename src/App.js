8ed35ee380be7fb61d5625df13fc454ccd260b71
Gennadii <atlantagenafes@gmail.com>
2022-07-02 15:33:05 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import logo from './atlanta_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <video preload="auto" autoPlay loop muted playsInline
+               src="https://www.noble-royale.com/wp-content/uploads/2021/05/Мой_фильм_2.mp4"
+               className="headerVideo"/>
+        <div className='wrap'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Atlanta
+          </p>
+          <a
+            className="App-link"
+            href="https://atlanta.ua"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Buildings
+          </a>
+        </div>
+
       </header>
     </div>
   );
