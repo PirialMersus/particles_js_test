d7228f9c8077633b8e97946c1b2f65dba08d4b05
Gennadii <atlantagenafes@gmail.com>
2022-07-04 17:58:16 +0300

    video added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import logo from './atlanta_logo.png';
 import './App.css';
+import vid from '../src/assets/BlackBrushStrokeTransition.mp4';
+import {useEffect, useRef} from "react";
+
 
 function App() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    videoRef.current.pause()
+    const onScroll = () => {
+      videoRef.current.pause()
+    };
+
+    setInterval(function(){
+      videoRef.current.currentTime = Math.ceil(window.scrollY)/900;
+    }, 20);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <video preload="auto" autoPlay loop muted playsInline
-               src="https://www.noble-royale.com/wp-content/uploads/2021/05/Мой_фильм_2.mp4"
-               className="headerVideo"/>
+        <video
+          ref={videoRef}
+          preload="auto"
+          autoPlay={false}
+          loop
+          muted
+          playsInline
+          src={vid}
+          className="headerVideo"/>
         <div className='wrap'>
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <p>
             Atlanta
           </p>
